998bcd124068f8ce0115b3d6a118a2a91061afff
sovofeel <sovofeel@gamil.com>
2018-09-12 13:57:17 +0500

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,16 @@ module.exports = {
       {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
       }
     ]
   },
